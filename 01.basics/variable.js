e27f9ab0955686ef0292491b9f3ecd99531799d5
Aridaman Singh <115462204+Aridaman91@users.noreply.github.com>
2024-08-28 09:41:08 +0000

    config

---
+++ 01.basics/variable.js
@@ -0,0 +1,12 @@
+const accoundid=(213123);
+let accountEmail=("aridamansingh@gamil.com");
+var accountpassword =("234234");
+accountcity="lucknow";
+
+
+
+accountEmail="hitesh@gmial.com"
+accountpassword=3423423
+accountcity="jaipur"
+console.log([accountEmail,accoundid,accountpassword,accountcity]);
+console.table([accountEmail,accoundid,accountpassword,accountcity]);
